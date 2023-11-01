08dd60d43b74f10089982089326f163dc6d20e39
RODRIGO CARGNELUTTI <rodrigo.cargnelutti@ufsm.br>
2023-11-01 14:43:55 -0300

    Tempo decorrido

--- exemplos-codigo/solucao_ingenua.py
+++ exemplos-codigo/solucao_ingenua.py
@@ -1,3 +1,5 @@
+import time
+
 dataset = [
     {"Título": "Artigo 1", "Resumo": "Resumo do Artigo 1. Ele fala sobre algoritmos."},
     {"Título": "Artigo 2", "Resumo": "Resumo do Artigo 2, que explora o design de algoritmos eficientes."},
@@ -26,6 +28,8 @@ palavras_chave = ["algoritmo", "design", "eficiente"]
 # Inicialização de um dicionário para armazenar o número de correspondências para cada entrada do dataset
 correspondencias = {}
 
+tempo_inicial = time.time() # em segundos
+
 # Algoritmo de String Matching
 for i, entrada in enumerate(dataset):               # Percorre os registros do dataset
     correspondencias[i] = 0
@@ -35,9 +39,14 @@ for i, entrada in enumerate(dataset):               # Percorre os registros do d
         if palavra in titulo or palavra in resumo:  # Verifica se palavra-chave consta no titulo ou no resumo
             correspondencias[i] += 1                # Incrementa o número de correspondências
 
+tempo_final = time.time() # em segundos
+
 # Classificação e ordenação com base no número de correspondências
 resultado_ordenado = sorted(correspondencias, key=lambda x: correspondencias[x], reverse=True)
 
 # Exibição dos resultados ordenados
 for i in resultado_ordenado:
     print(f"Classificação: {correspondencias[i]} - Título: {dataset[i]['Título']}, Resumo: {dataset[i]['Resumo']}")
+
+#Print do tempo que demorou para rodar a parte específica do código
+print(f"Duração: {tempo_final - tempo_inicial} segundos")
